336acb60d92f18fa5aee1dc94ca2a50a1c83f64e
Emmanuel-Castro <castroenmanuel98@gmail.com>
2025-08-23 21:41:52 -0400

    fix: Remove files copies to clean the project's structure

--- loan-calculator.js
+++
@@ -1,7 +0,0 @@
-//Button event for testing issues
-
-const button = document.getElementById('loanCalculatorButton');
-
-button.addEventListener("click", function() {
-    window.alert("Test Event");
-});
